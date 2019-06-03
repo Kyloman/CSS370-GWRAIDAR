98df36b967f06f3b05c8e7579bdbdb065df267ee
mckinleymelton <mcmelton@uw.edu>
2019-06-03 09:32:53 -0700

    Update GeneralEncounterDatabase.py

--- GeneralEncounterDatabase.py
+++ GeneralEncounterDatabase.py
@@ -4,7 +4,29 @@ class GeneralEncounterDatabase:
         pass
     def getEncounterList():
         pass
+    
+class Level0DataAccess:
+    def viewData():
+        pass
+  
+class Level1DataAccess:
+    def viewData():
+        pass
+    def UploadData():
+        pass 
+   
+class Level2DataAccess:
+    def viewData():
+        pass
+    def UploadData():
+        pass
+    def ModifyData():
+        pass
 
 """    Provided Interface
 List<GeneralEncounter> getGeneralEncounterList();
-GeneralDetails getGeneralEncounterDetails(GeneralEncounter ge);"""
+GeneralDetails getGeneralEncounterDetails(GeneralEncounter ge);
+
+3 Levels of security access for viewing the data as part of the
+Security strategy
+"""
