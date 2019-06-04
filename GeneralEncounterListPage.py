c30c74f40875f9bc004cbff58731960e6b1848e7
Kaya Peng p-mac <pengkaya09@gmail.com>
2019-06-03 23:42:48 -0700

    fixed error

--- GeneralEncounterListPage.py
+++ GeneralEncounterListPage.py
@@ -4,16 +4,10 @@ class GeneralEncounterListPage:
 	
     def display():
         pass
-<<<<<<< HEAD
     
     # user passes in the encounter log file and the filter would filter out the character's magic find and calculates the drop rate items for each boss
 	def encounterFilter():
 		pass    
-=======
-
-    def encounterFilter():
-        pass    
->>>>>>> 79c6d4ea7444a845073d43c4a52df05ecf726755
 
 """
 List<Encounter> getEncounterList();
