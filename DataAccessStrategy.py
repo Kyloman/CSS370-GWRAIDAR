18802abb421575a5a43dbe974bc985cb1db9c09f
mckinleymelton <mcmelton@uw.edu>
2019-06-06 23:27:22 -0700

    Update DataAccessStrategy.py

--- DataAccessStrategy.py
+++ DataAccessStrategy.py
@@ -1,1 +1,25 @@
-
+class DataAccessStrategy:
+  def getStrategy(int):
+    pass
+  
+class Level0AccountAccess:
+  def viewAccount():
+    pass
+  def modifyAccount():
+    pass
+  
+class Level2AccountAccess:
+  def viewAccount():
+    pass
+  def modifyAccount():
+    pass
+  def deleteAccount():
+    pass
+  
+"""
+Security Strategy:
+This class handles the initial implementation of
+the security strategy.
+The remaining classes are implemented in the existing
+files
+"""
