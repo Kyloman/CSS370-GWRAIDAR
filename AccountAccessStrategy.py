ac31c67b2956529ccd9faecca8de91d7b0b167c1
mckinleymelton <mcmelton@uw.edu>
2019-06-06 23:37:28 -0700

    Create AccountAccessStrategy.py

---
+++ AccountAccessStrategy.py
@@ -0,0 +1,17 @@
+class AccountAccessStrategy:
+  def getStrategy(int):
+    pass
+
+class Level0AccountAccess:
+  def viewAccount():
+    pass
+  def modifyAccount:
+    pass
+    
+class Level2AccountAccess:
+  def viewAccount():
+    pass
+  def modifyAccount():
+    pass
+  def deleteAccount():
+    pass
